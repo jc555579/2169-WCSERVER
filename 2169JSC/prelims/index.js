e9c50b037c7130427e3d33cc233ffedaabfad996
Joemar Catipon <112145613+jc555579@users.noreply.github.com>
2025-07-04 17:15:41 +0800

    Delete 2169JSC/prelims/index.js

--- 2169JSC/prelims/index.js
+++
@@ -1,9 +0,0 @@
-//index.js
-// var myLogModule = require('./PRELIMS/utility/log.js')
-
-// myLogModule.warning('Warning node not configured..');
-// myLogModule.error('Error: Node encountered an error..');
-// myLogModule.info('Node running...');
-
-var msg = require('./utility/Messages.js');
-console.log(msg);
